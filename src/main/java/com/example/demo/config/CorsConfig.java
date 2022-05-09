4e72ee97e374725099b154bd6df91c62b9652fdb
Akhil Jinu <portforwarding03@gmail.com>
2022-05-10 00:10:40 +0530

    add cors

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		
+		return new WebMvcConfigurer() {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("GET","POST","DELETE","PUT")
+				.allowedHeaders("*")
+				.allowedOrigins("http://localhost:3001","http://localhost:3000");
+			}
+		};
+		
+	}
+
+}
